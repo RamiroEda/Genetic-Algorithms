10a63de86c632a0c1e24b6c1916d429bbda77830
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2021-03-10 14:18:58 -0600

    Agregados nuevas maneras de construir el objeto

--- src/main/kotlin/Phenotype.kt
+++ src/main/kotlin/Phenotype.kt
@@ -1,10 +1,13 @@
+import kotlin.random.Random
+
 class Phenotype(
-    val value: Long
+    val chromosome : Long
 ) {
-    val fitness : Long
-        get() = value * value
+    val fitness = chromosome.times(chromosome)
 
     fun cross(other: Phenotype, mask: String) = Companion.cross(this, other, mask)
+
+    fun clone() = fromPhenotype(this)
 
     companion object {
         fun cross(
@@ -14,24 +17,31 @@ class Phenotype(
         ) : Pair<Phenotype, Phenotype> {
             val maskLong = mask.binaryStringToLong()
 
-            val activatedP1 = phenotype1.value.and(maskLong)
-            val activatedP2 = phenotype2.value.and(maskLong)
+            val activatedP1 = phenotype1.chromosome.and(maskLong)
+            val activatedP2 = phenotype2.chromosome.and(maskLong)
 
-            val deactivatedP1 = phenotype1.value.and(maskLong.inv())
-            val deactivatedP2 = phenotype2.value.and(maskLong.inv())
+            val deactivatedP1 = phenotype1.chromosome.and(maskLong.inv())
+            val deactivatedP2 = phenotype2.chromosome.and(maskLong.inv())
 
             val v1 = activatedP1.or(deactivatedP2)
             val v2 = activatedP2.or(deactivatedP1)
 
             return Pair(Phenotype(v1), Phenotype(v2))
         }
+
+        fun fromPhenotype(
+            phenotype: Phenotype
+        ) = Phenotype(phenotype.chromosome)
+
+        fun random() = Phenotype(Random.nextLong())
     }
 
     override fun toString() = """
         ---------------------------------------------
-        genotype = ${value.toString(2)}
+        phenotype       = $chromosome
+        genotype        = ${chromosome.toString(2)}
         
-        fitness = $fitness
+        fitness         = $fitness
         ---------------------------------------------
     """.trimIndent()
 }
